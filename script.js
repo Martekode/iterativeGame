51e6bac9e0e86784e13fa2cd965b8dd2f6c00e45
Martekode <BrianDesmedt9000@hotmail.com>
2022-05-16 10:46:22 +0200

    added checker for first upgrade, becomes green and rmv disabled class

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 //global variables
+    //variables for the clicker itself
 var scorePlace = document.getElementById('score');
 var scoreClicker = parseInt(scorePlace.innerText);
 var incClicker = 1;
+    //vars for buy
+var buySlightOfHandPlace = document.getElementById('slightOfHand');
+var buySlightOfHandNumb = parseInt(buySlightOfHandPlace.innerText);
 //clicker function
 document.getElementById('mainImg').addEventListener('click', function(){
-    //console.log(scoreClicker);
+    //Clicker
     scoreClicker += incClicker;
     scorePlace.innerText = scoreClicker;
-    //console.log(scoreClicker);
-})
+    //buy checker for 1st upgrade
+    if(scoreClicker >= buySlightOfHandNumb){
+        buySlightOfHandPlace.classList.remove('disabled');
+        buySlightOfHandPlace.style.backgroundColor = "green";
+        buySlightOfHandPlace.style.borderColor = "lightgreen"
+    }
+});
+
