83fa78a4e7dcd67255954f22bfda759a9e381faa
Martekode <BrianDesmedt9000@hotmail.com>
2022-05-16 11:44:22 +0200

    added cost incr and cost affects score

--- script.js
+++ script.js
@@ -7,13 +7,7 @@ var incClicker = 1;
         //slightOfHand
 var buySlightOfHandPlace = document.getElementById('slightOfHand');
 var buySlightOfHandNumb = parseInt(buySlightOfHandPlace.innerText);
-//clicker function
-document.getElementById('mainImg').addEventListener('click', function(){
-    //Clicker
-    scoreClicker += incClicker;
-    scorePlace.innerText = scoreClicker;
-    
-});
+var upgradeScore = buySlightOfHandNumb;
 //checker loop
 setInterval(function(){
     if(scoreClicker >= buySlightOfHandNumb){
@@ -21,5 +15,30 @@ setInterval(function(){
         buySlightOfHandPlace.style.backgroundColor = "green";
         buySlightOfHandPlace.style.borderColor = "lightgreen"
     }
+    else{
+        buySlightOfHandPlace.classList.add('disabled');
+    }
     console.log('updating');
 },1000)
+//clicker function
+document.getElementById('mainImg').addEventListener('click', function(){
+    //Clicker
+    scoreClicker += incClicker;
+    scorePlace.innerText = scoreClicker;
+    
+});
+//upgradepacks 1st upgrade
+document.getElementById('slightOfHand').addEventListener('click',function(e){
+    //checker resuirements
+    e.preventDefault;
+    if(scoreClicker >= upgradeScore){
+        scoreClicker = scoreClicker-upgradeScore;
+        upgradeScore = upgradeScore * 2;
+        buySlightOfHandNumb = upgradeScore ;
+        buySlightOfHandPlace.innerText = buySlightOfHandNumb;
+        incClicker = incClicker *2;
+        console.log(upgradeScore)
+        console.log(incClicker)
+        console.log(buySlightOfHandPlace.innerText)
+    }
+})
